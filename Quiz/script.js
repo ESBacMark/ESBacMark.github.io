3d572596621fd33e6a0892f767133e2986208463
ESBacMark <teemu.hinkula@gmail.com>
2022-06-08 22:39:00 +0200

    QuizUpdate

---
+++ Quiz/script.js
@@ -0,0 +1,370 @@
+document.onclick = function(){
+	var correctAnss = 0;
+	//Question 1
+	if(document.getElementById("q1wrong1").checked == true){
+		document.getElementById("resultQ1").innerHTML = "Wrong! "
+	document.getElementById("Question1").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q1wrong2").checked == true){
+		document.getElementById("resultQ1").innerHTML = "Not that bad, but wrong!"
+		document.getElementById("Question1").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q1correct").checked == true){
+		document.getElementById("resultQ1").innerHTML = "Correct!"
+		document.getElementById("Question1").style.backgroundColor = "green";
+	correctAnss++
+	}
+	
+	//Question 2
+	if(document.getElementById("q2wrong1").checked == true){
+		document.getElementById("resultQ2").innerHTML = "Wrong!"
+		document.getElementById("Question2").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q2correct").checked == true){
+		document.getElementById("resultQ2").innerHTML = "Correct!"
+		document.getElementById("Question2").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q2wrong2").checked == true){
+		document.getElementById("resultQ2").innerHTML = "Wrong!"
+		document.getElementById("Question2").style.backgroundColor = "red";
+	}
+	//Question 3
+	if(document.getElementById("q3wrong1").checked == true){
+		document.getElementById("resultQ3").innerHTML = "Wrong!"
+		document.getElementById("Question3").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q3correct").checked == true){
+		document.getElementById("resultQ3").innerHTML = "Correct!"
+		document.getElementById("Question3").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q3wrong2").checked == true){
+		document.getElementById("resultQ3").innerHTML = "Wrong!"
+		document.getElementById("Question3").style.backgroundColor = "red";
+	}
+		//Question 4
+	if(document.getElementById("q4wrong1").checked == true){
+		document.getElementById("resultQ4").innerHTML = "Wrong!"
+		document.getElementById("Question4").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q4correct").checked == true){
+		document.getElementById("resultQ4").innerHTML = "Correct!"
+		document.getElementById("Question4").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q4wrong2").checked == true){
+		document.getElementById("resultQ4").innerHTML = "Wrong!"
+		document.getElementById("Question4").style.backgroundColor = "red";
+	}
+			//Question 5
+	if(document.getElementById("q5wrong1").checked == true){
+		document.getElementById("resultQ5").innerHTML = "Wrong!"
+		document.getElementById("Question5").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q5correct").checked == true){
+		document.getElementById("resultQ5").innerHTML = "Correct!"
+		document.getElementById("Question5").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q5wrong2").checked == true){
+		document.getElementById("resultQ5").innerHTML = "Wrong!"
+		document.getElementById("Question5").style.backgroundColor = "red";
+	}
+	//Question 6
+	if(document.getElementById("q6wrong1").checked == true){
+		document.getElementById("resultQ6").innerHTML = "Wrong!"
+		document.getElementById("Question6").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q6correct").checked == true){
+		document.getElementById("resultQ6").innerHTML = "Correct!"
+		document.getElementById("Question6").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q6wrong2").checked == true){
+		document.getElementById("resultQ6").innerHTML = "Wrong!"
+		document.getElementById("Question6").style.backgroundColor = "red";
+	}
+	//Question 7
+	if(document.getElementById("q7wrong1").checked == true){
+		document.getElementById("resultQ7").innerHTML = "Wrong!"
+		document.getElementById("Question7").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q7correct").checked == true){
+		document.getElementById("resultQ7").innerHTML = "Correct!"
+		document.getElementById("Question7").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q7wrong2").checked == true){
+		document.getElementById("resultQ7").innerHTML = "Wrong!"
+		document.getElementById("Question7").style.backgroundColor = "red";
+	}
+	//Question 8
+	if(document.getElementById("q8wrong1").checked == true){
+		document.getElementById("resultQ8").innerHTML = "Wrong!"
+		document.getElementById("Question8").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q8correct").checked == true){
+		document.getElementById("resultQ8").innerHTML = "Correct!"
+		document.getElementById("Question8").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q8wrong2").checked == true){
+		document.getElementById("resultQ8").innerHTML = "Wrong!"
+		document.getElementById("Question8").style.backgroundColor = "red";
+	}
+	//Question 9
+	if(document.getElementById("q9wrong1").checked == true){
+		document.getElementById("resultQ9").innerHTML = "Wrong!"
+		document.getElementById("Question9").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q9correct").checked == true){
+		document.getElementById("resultQ9").innerHTML = "Correct!"
+		document.getElementById("Question9").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q9wrong2").checked == true){
+		document.getElementById("resultQ9").innerHTML = "Wrong!"
+		document.getElementById("Question9").style.backgroundColor = "red";
+	}
+	//Question 10
+	if(document.getElementById("q10wrong1").checked == true){
+		document.getElementById("resultQ10").innerHTML = "Wrong!"
+		document.getElementById("Question10").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q10correct").checked == true){
+		document.getElementById("resultQ10").innerHTML = "Correct!"
+		document.getElementById("Question10").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q10wrong2").checked == true){
+		document.getElementById("resultQ10").innerHTML = "Wrong!"
+		document.getElementById("Question10").style.backgroundColor = "red";
+	}
+	//Question 11
+	if(document.getElementById("q11wrong1").checked == true){
+		document.getElementById("resultQ11").innerHTML = "Wrong!"
+		document.getElementById("Question11").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q11correct").checked == true){
+		document.getElementById("resultQ11").innerHTML = "Correct!"
+		document.getElementById("Question11").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q11wrong2").checked == true){
+		document.getElementById("resultQ11").innerHTML = "Wrong!"
+		document.getElementById("Question11").style.backgroundColor = "red";
+	}
+	//Question 12
+	if(document.getElementById("q12wrong1").checked == true){
+		document.getElementById("resultQ12").innerHTML = "Wrong!"
+		document.getElementById("Question12").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q12correct").checked == true){
+		document.getElementById("resultQ12").innerHTML = "Correct!"
+		document.getElementById("Question12").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q12wrong2").checked == true){
+		document.getElementById("resultQ12").innerHTML = "Wrong!"
+		document.getElementById("Question12").style.backgroundColor = "red";
+	}
+	//Question 13
+	if(document.getElementById("q13wrong1").checked == true){
+		document.getElementById("resultQ13").innerHTML = "Wrong!"
+		document.getElementById("Question13").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q13correct").checked == true){
+		document.getElementById("resultQ13").innerHTML = "Correct!"
+		document.getElementById("Question13").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q13wrong2").checked == true){
+		document.getElementById("resultQ13").innerHTML = "Wrong!"
+		document.getElementById("Question13").style.backgroundColor = "red";
+	}
+	//Question 14
+	if(document.getElementById("q14wrong1").checked == true){
+		document.getElementById("resultQ14").innerHTML = "Wrong!"
+		document.getElementById("Question14").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q14correct").checked == true){
+		document.getElementById("resultQ14").innerHTML = "Correct!"
+		document.getElementById("Question14").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q14wrong2").checked == true){
+		document.getElementById("resultQ14").innerHTML = "Wrong!"
+		document.getElementById("Question14").style.backgroundColor = "red";
+	}
+	//Question 15
+	if(document.getElementById("q15wrong1").checked == true){
+		document.getElementById("resultQ15").innerHTML = "Wrong!"
+		document.getElementById("Question15").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q15correct").checked == true){
+		document.getElementById("resultQ15").innerHTML = "Correct!"
+		document.getElementById("Question15").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q15wrong2").checked == true){
+		document.getElementById("resultQ15").innerHTML = "Wrong!"
+		document.getElementById("Question15").style.backgroundColor = "red";
+	}
+	//Question 16
+	if(document.getElementById("q16wrong1").checked == true){
+		document.getElementById("resultQ16").innerHTML = "Wrong!"
+		document.getElementById("Question16").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q16correct").checked == true){
+		document.getElementById("resultQ16").innerHTML = "Correct!"
+		document.getElementById("Question16").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q16wrong2").checked == true){
+		document.getElementById("resultQ16").innerHTML = "Wrong!"
+		document.getElementById("Question16").style.backgroundColor = "red";
+	}
+	//Question 17
+	if(document.getElementById("q17wrong1").checked == true){
+		document.getElementById("resultQ17").innerHTML = "Wrong!"
+		document.getElementById("Question17").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q17correct").checked == true){
+		document.getElementById("resultQ17").innerHTML = "Correct!"
+		document.getElementById("Question17").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q17wrong2").checked == true){
+		document.getElementById("resultQ17").innerHTML = "Wrong!"
+		document.getElementById("Question17").style.backgroundColor = "red";
+	}
+	//Question 18
+	if(document.getElementById("q18wrong1").checked == true){
+		document.getElementById("resultQ18").innerHTML = "Wrong!"
+		document.getElementById("Question18").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q18correct").checked == true){
+		document.getElementById("resultQ18").innerHTML = "Correct!"
+		document.getElementById("Question18").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q18wrong2").checked == true){
+		document.getElementById("resultQ18").innerHTML = "Wrong!"
+		document.getElementById("Question18").style.backgroundColor = "red";
+	}
+	//Question 19
+	if(document.getElementById("q19wrong1").checked == true){
+		document.getElementById("resultQ19").innerHTML = "Wrong!"
+		document.getElementById("Question19").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q19correct").checked == true){
+		document.getElementById("resultQ19").innerHTML = "Correct!"
+		document.getElementById("Question19").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q19wrong2").checked == true){
+		document.getElementById("resultQ19").innerHTML = "Wrong!"
+		document.getElementById("Question19").style.backgroundColor = "red";
+	}
+	//Question 20
+	if(document.getElementById("q20wrong1").checked == true){
+		document.getElementById("resultQ20").innerHTML = "Wrong!"
+		document.getElementById("Question20").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q20correct").checked == true){
+		document.getElementById("resultQ20").innerHTML = "Correct!"
+		document.getElementById("Question20").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q20wrong2").checked == true){
+		document.getElementById("resultQ20").innerHTML = "Wrong!"
+		document.getElementById("Question20").style.backgroundColor = "red";
+	}
+	//Question 21
+	if(document.getElementById("q21wrong1").checked == true){
+		document.getElementById("resultQ21").innerHTML = "Wrong!"
+		document.getElementById("Question21").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q21correct").checked == true){
+		document.getElementById("resultQ21").innerHTML = "Correct!"
+		document.getElementById("Question21").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q21wrong2").checked == true){
+		document.getElementById("resultQ21").innerHTML = "Wrong!"
+		document.getElementById("Question21").style.backgroundColor = "red";
+	}
+	//Question 22
+	if(document.getElementById("q22wrong1").checked == true){
+		document.getElementById("resultQ22").innerHTML = "Wrong!"
+		document.getElementById("Question22").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q22correct").checked == true){
+		document.getElementById("resultQ22").innerHTML = "Correct!"
+		document.getElementById("Question22").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q22wrong2").checked == true){
+		document.getElementById("resultQ22").innerHTML = "Wrong!"
+		document.getElementById("Question22").style.backgroundColor = "red";
+	}
+	//Question 23
+	if(document.getElementById("q23wrong1").checked == true){
+		document.getElementById("resultQ23").innerHTML = "Wrong!"
+		document.getElementById("Question23").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q23correct").checked == true){
+		document.getElementById("resultQ23").innerHTML = "Correct!"
+		document.getElementById("Question23").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q23wrong2").checked == true){
+		document.getElementById("resultQ23").innerHTML = "Wrong!"
+		document.getElementById("Question23").style.backgroundColor = "red";
+	}
+	//Question 24
+	if(document.getElementById("q24wrong1").checked == true){
+		document.getElementById("resultQ24").innerHTML = "Wrong!"
+		document.getElementById("Question24").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q24correct").checked == true){
+		document.getElementById("resultQ24").innerHTML = "Correct!"
+		document.getElementById("Question24").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q24wrong2").checked == true){
+		document.getElementById("resultQ24").innerHTML = "Wrong!"
+		document.getElementById("Question24").style.backgroundColor = "red";
+	}
+	//Question 25
+	if(document.getElementById("q25wrong1").checked == true){
+		document.getElementById("resultQ25").innerHTML = "Wrong!"
+		document.getElementById("Question25").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q25correct").checked == true){
+		document.getElementById("resultQ25").innerHTML = "Correct!"
+		document.getElementById("Question25").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q25wrong2").checked == true){
+		document.getElementById("resultQ25").innerHTML = "Wrong!"
+		document.getElementById("Question25").style.backgroundColor = "red";
+	}
+	//Question 26
+	if(document.getElementById("q26wrong1").checked == true){
+		document.getElementById("resultQ26").innerHTML = "Wrong!"
+		document.getElementById("Question26").style.backgroundColor = "red";
+	}
+	if(document.getElementById("q26correct").checked == true){
+		document.getElementById("resultQ26").innerHTML = "Correct!"
+		document.getElementById("Question26").style.backgroundColor = "green";
+		correctAnss++
+	}
+	if(document.getElementById("q26wrong2").checked == true){
+		document.getElementById("resultQ26").innerHTML = "Wrong!"
+		document.getElementById("Question26").style.backgroundColor = "red";
+	}
+	document.getElementById("feedback").innerHTML = "Hello "+name+". You had "+correctAnss+" correct answers!" 
+	
+}
